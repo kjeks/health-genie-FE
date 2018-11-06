1fa4d7d266ba7a1829880f6b1c705e00bf8bd980
kjeks <danskejon@hotmail.com>
2018-11-06 08:18:15 +0100

    Fix requests to backend

--- src/common/requestUtil.js
+++ src/common/requestUtil.js
@@ -1,10 +1,7 @@
 export function makeRequest({url, method, body}) {
     const token = localStorage.getItem('token') || "no token";
-    console.log(process.env);
 
-
-
-    return fetch(`${process.env.REACT_APP_SERVER_URL}/${url}`, {
+    return fetch(`${process.env.REACT_APP_SERVER_URL}/api/${url}`, {
         body: JSON.stringify(body),
         headers: {
             'content-type': 'application/json',
